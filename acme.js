586d95659af261fdb1d31ee24dc61e1ec44b2067
yonatan molla <jhoni42002@yahoo.com>
2016-08-19 16:04:37 -0500

    made my code more compact

--- acme.js
+++ acme.js
@@ -1,82 +1,57 @@
-var products = [];
-var types = [];
+'use strict';
+
 var categories = [];
+var types = [];
+var products = [];
 
 
 
 $(document).ready(function() {
+  $.getJSON('categories.json' )
+    .then((data1) => {
+      console.log('data1', data1)
+      categories = data1.categories;
+      // return categories
+      // displayCategories(data1);
+      return displayCategories(categories);
 
-    var loadCategories = function() {
-        return new Promise((resolve, reject) => {
-            $.ajax({ url: "categories.json"
-            }).done((data) => {
-                resolve(data.categories);
-            }).fail((xhr, status, error) => {
-                reject(error);
-            });
-        })
-    };
+    })
 
-    var loadTypes = function(cb1) {
-        return new Promise((resolve, reject) => {
-            $.ajax({ url: "types.json",
-                data: cb1
-            }).done((data) => {
-                resolve(data.types);
-            }).fail((xhr, status, error) => {
-                reject(error);
-            });
-        })
-    };
+      $.getJSON('types.json')
+      .then((data2) => {
+         types = data2.types;
+         // $("select").change(function (event) {
+          return displayProduct(types);
+      })
 
-    var loadProducts = function(cb2) {
-        return new Promise((resolve, reject) => {
-            $.ajax({ url: "products.json",
-                data: cb2
-            }).done((data) => {
-                resolve(data.products);
-            }).fail((xhr, status, error) => {
-                reject(error);
-            });
-        })
-    };
+      $.getJSON('products.json')
+      .then((data3) => {
+         products = data3.products;
+           return displayProduct(products);
+        // console.log('data3', products)
+      })
 
-    loadCategories()
-        .then((data1) => {
-            categories = data1;
-            displayCategories(categories);
-            return loadTypes(data1);
-        })
-        .then((data2) => {
-            types = data2;
-            return loadProducts(data2);
-        })
-        .then((data3) => {
-            products = data3;
-            displayProduct(data3);
-        })
 
 
   var menu = $('#dropDown1');
 
     function displayCategories(data1) {
-        data1.forEach(function(categories) {
-            // menu = `<option value="${categories.id}" text="${categories.name}"></option>`
+        categories.forEach(function(categories) {
             $('<option />', {value: categories.id,
                              text: categories.name}).appendTo(menu);
         }) //append() to is different from appendTo()
-
     };
-    $("select").on("change", function(event) {
+    //     // console.log("this", $("option:selected").text())
+
+$("select").on("change", function(event) {
         var currentChoice = this.value;
-        console.log("this", $(this).find("option:selected").text())
         displayProduct(currentChoice);
 
     })
-
     function displayProduct(picked) {
+        // console.log("change happened")
         $("#output").empty();
-            products.forEach(function(products) {
+            products.forEach(function (products) {
               var currentTypeId = products.type;
               var currentCategoryId = types[currentTypeId].category;
               if(currentCategoryId == picked){
